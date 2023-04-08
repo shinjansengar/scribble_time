1f1b7efaae979ddbe163c4561a7a52373f11286a
=Shinjan sengar <=shinjansengar@gmail.com>
2023-04-08 09:47:02 +0530

    Frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import DrawingBoard from "./components/drawingBoard";
+
+function App() {
+  return (
+    <div className="App">
+      <DrawingBoard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/drawingBoard.js
@@ -0,0 +1,7 @@
+const DrawingBoard = () =>{
+    return(
+        <h1>Board</h1>
+    )
+}
+
+export default DrawingBoard;
