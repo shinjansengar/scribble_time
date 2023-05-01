69ca8b705a032dca436cbc51835f7b66387c8650
=Shinjan sengar <=shinjansengar@gmail.com>
2023-05-01 20:27:35 +0530

    User schema changed

--- server/models/user.js
+++ server/models/user.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema({
     score:{
         type: Number,
         default: 0
+    },
+    isDrawer:{
+        type: Boolean,
+        default: false
     }
 })
 
